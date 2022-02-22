98ad152b77aaea7fa631f5da34439827c704e8b2
Abe Caymo <abe@zaltek.co.uk>
2022-02-22 14:09:22 +0800

    add images

---
+++ src/images/category-icons/index.js
@@ -0,0 +1,17 @@
+import design from "./paint.svg";
+import marketing from "./promotion.svg";
+import nonTech from "./non-tech.svg";
+import software from "./programming.svg";
+import virtualAssistant from "./virtual-assistant.svg";
+import writing from "./writer.svg";
+import teaching from "./teacher.svg";
+
+export {
+  design,
+  marketing,
+  nonTech,
+  software,
+  virtualAssistant,
+  writing,
+  teaching,
+};
